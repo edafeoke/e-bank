d3c95f07d6fb12cc751b6658c887a0d32535d605
Edafe Oke <greatedafeoke@gmail.com>
2022-05-29 22:47:00 +0100

    Android web

--- frontend/web/src/App.js
+++ frontend/web/src/App.js
@@ -1,14 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-import LoginPage from './admin/pages/LoginPage';
-import Header from './components/Header';
+import "./App.css";
+import { Form, Button, ThemeProvider } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className='container body'>
-      <Header/>
-      <LoginPage/>
-    </div>
+    <ThemeProvider
+      breakpoints={["xxxl", "xxl", "xl", "lg", "md", "sm", "xs", "xxs"]}
+    >
+      <div className="container fluid">
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      </div>
+    </ThemeProvider>
   );
 }
 
